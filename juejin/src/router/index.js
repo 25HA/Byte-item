2983f909eb909985c1c79dbfe8197520c05ae3e8
无尘 <614208292@qq.com>
2022-08-11 21:56:44 +0800

    index-tabbar

--- juejin/src/router/index.js
+++ juejin/src/router/index.js
@@ -14,6 +14,11 @@ const routes = [
     path: '/about',
     name: 'about',
     component: () => import('../views/AboutView.vue')
+  },
+  {
+    path: '/content',
+    name: 'content',
+    component: () => import('../views/Content.vue')
   }
 ]
 
---
+++ juejin/src/views/Content.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="content">
+    <h1>内容页面</h1>
+  </div>
+</template>
