55d1cdad03017dcc4a414f6c539555d87ead3603
CHW-harvey <2633368044@qq.com>
2022-08-24 09:18:09 +0800

    1

--- juejin/src/router/index.js
+++ juejin/src/router/index.js
@@ -1,9 +1,4 @@
 import Vue from 'vue'
-<<<<<<< HEAD
-=======
-// import Element from 'element-ui'
-// import 'element-ui/lib/theme-chalk/index.css'
->>>>>>> 233d2576127a32ec67a142f5b8d6589defa3a72f
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import test from '../views/boilingView.vue'
